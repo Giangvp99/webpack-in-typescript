c8ec925726093f6c264c661a50740d0a40ef664d
Giangvp99 <truonggiangvp99@gmail.com>
2021-04-12 21:59:56 +0300

    config webpack for ts

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    app: ["./src/index.tsx"],
+    vendor: ["react", "react-dom"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].bundle.js",
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/assets/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles/styles.css",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  optimization: {
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+  },
+  devServer: {
+    port: 9000,
+    open: true,
+    historyApiFallback: true,
+    publicPath: "/",
+  },
+};
